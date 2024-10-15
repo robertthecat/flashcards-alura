6bb719bcecc2e9907bf13d62af533d79e9d2c541
robert o gato <147512005+robertthecat@users.noreply.github.com>
2024-10-15 08:48:20 -0300

    2-6  7:30

---
+++ flashcards/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'culinaria',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
